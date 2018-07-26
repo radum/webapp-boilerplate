2548a0ee988327d19e4dd2956b23969f713b684c
RaduM <r.micu@amaze.com>
2018-07-26 16:00:23 +0100

    Imagemin task updated to generate webp files also

--- tools/tasks/imagemin.js
+++ tools/tasks/imagemin.js
@@ -3,11 +3,13 @@
 const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminOptipng = require('imagemin-optipng');
+const imageminWebp = require('imagemin-webp');
 const globby = require('globby');
 const os = require('os');
 const prettyBytes = require('pretty-bytes');
-const chalk = require('chalk');
 const pMap = require('p-map');
+const replaceExt = require('replace-ext');
+const chalk = require('chalk');
 
 const config = require('../config');
 const fs = require('../lib/fs');
@@ -19,7 +21,7 @@ async function imageminTask(options) {
 
 	const files = globby.sync(config.paths.imagesPath + '/**/*.{jpg,jpeg,png}');
 
-	const plugins = [
+	const pluginsDefault = [
 		imageminMozjpeg({
 			quality: 80,
 			progressive: true
@@ -27,22 +29,29 @@ async function imageminTask(options) {
 		imageminOptipng()
 	];
 
+	const pluginsWebP = [
+		imageminWebp({
+			quality: 75,
+			method: 6
+		})
+	];
+
 	let totalBytes = 0;
 	let totalSavedBytes = 0;
 	let totalFiles = 0;
 
-	const processFile = file => fs.readFile(file, { encoding: null })
+	const processFile = (file, plugins, opts) => fs.readFile(file, { encoding: null })
 		.then(buf => Promise.all([imagemin.buffer(buf, { plugins }), buf]))
-		.then((res) => {
-			// TODO: Use destructuring when targeting Node.js 6
-			const optimizedBuf = res[0];
-			const originalBuf = res[1];
+		.then((imageminBuf) => {
+			const optimizedBuf = imageminBuf[0];
+			const originalBuf = imageminBuf[1];
 			const originalSize = originalBuf.length;
 			const optimizedSize = optimizedBuf.length;
 			const saved = originalSize - optimizedSize;
 			const percent = originalSize > 0 ? (saved / originalSize) * 100 : 0;
 			const savedMsg = `saved ${prettyBytes(saved)} - ${percent.toFixed(1).replace(/\.0$/, '')}%`;
 			const msg = saved > 0 ? savedMsg : 'already optimized';
+			let destFile = file.replace(config.paths.imagesPath, config.paths.imagesOutputDest)
 
 			if (saved > 0) {
 				totalBytes += originalSize;
@@ -50,15 +59,26 @@ async function imageminTask(options) {
 				totalFiles += 1;
 			}
 
-			fs.writeFile(file.replace(config.paths.imagesPath, config.paths.imagesOutputDest), optimizedBuf);
-			logger.info(file + chalk.gray(` (${msg})`));
+			if (opts && opts.ext) {
+				destFile = replaceExt(destFile, opts.ext);
+			}
+
+			fs.writeFile(destFile, optimizedBuf);
+
+			logger.info(destFile + chalk.gray(` (${msg})`));
 		})
 		.catch((err) => {
 			logger.error(`${err} in file ${file}`);
 		});
 
+	const mapper = (file) => {
+		return Promise.all([
+			processFile(file, pluginsDefault),
+			processFile(file, pluginsWebP, { ext: '.webp' })
+		]);
+	};
 
-	return pMap(files, processFile, { concurrency: os.cpus().length }).then(() => {
+	return pMap(files, mapper, { concurrency: os.cpus().length }).then(() => {
 		const percent = totalBytes > 0 ? (totalSavedBytes / totalBytes) * 100 : 0;
 		let msg = `minified ${totalFiles} images`;
 
